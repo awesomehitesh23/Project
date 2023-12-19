16b633895c52d975be3e72431bc736eecbdb9b80
awesomehitesh23 <136333685+awesomehitesh23@users.noreply.github.com>
2023-12-19 18:54:48 +0530

    Updated Solution.java

--- src/DTO/Solution.java
+++ src/DTO/Solution.java
@@ -4,6 +4,7 @@ public class Solution {
   public static void main(String args[]) {
 	System.out.println("Hello World");
 	System.out.println("My Name is Hitesh");
+	  System.out.println("My Name is Gaurav");
 }
 
 }
